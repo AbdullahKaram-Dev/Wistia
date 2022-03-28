7bb3346484c14ce3addd6c959f25de7276698c46
Abdullah Karam <abdallakaramdev@gmail.com>
2022-03-28 20:33:19 +0200

    finish integration with wistia api

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 use GuzzleHttp\Client;
 require 'vendor/autoload.php';
 
-$key = 'Bearer 8c5bbd20b3600ce91f5fadda3a668cf267f3562c76daa21d5bae0846eefaf0a2';
+$key = '************************************************************';
 
 /** create new project */
 /*$project_data = [
