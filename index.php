476b03f232ecd47abe5cb133668f935f753460fb
Abdullah Karam <abdallakaramdev@gmail.com>
2022-03-28 17:47:24 +0200

    last

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $key = 'Bearer 8c5bbd20b3600ce91f5fadda3a668cf267f3562c76daa21d5bae0846eefaf0a2'
 
 /** create new project */
 /*$project_data = [
-'name' => 'PHP DEVELOPMENT',
-'adminEmail' => 'abdallakaramdev@gmail.com',
-'anonymousCanUpload' => true,
-'anonymousCanDownload ' => true
+'name' => '3NDO',
+'adminEmail' => 'abdelazem1581996@gmail.com',
+'anonymousCanUpload' => false,
+'anonymousCanDownload ' => false
 ];
 $json_data = json_encode($project_data);
 $httpClient = new Client([
@@ -83,7 +83,7 @@ echo '<iframe width="420" height="315" src="'.$finalResponse['assets'][0]['url']
 dd($finalResponse);*/
 
 /** update single media */
-$media_data_updated = [
+/*$media_data_updated = [
     'name' => 'updated media name',
     'description' => 'updated media description'
 ];
@@ -94,4 +94,40 @@ $httpClient = new Client([
 ]);
 $sendRequestAndGetResponse = $httpClient->put("https://api.wistia.com/v1/medias/tcg2euz6to.json");
 $finalResponse = json_decode($sendRequestAndGetResponse->getBody(), true);
-dd($finalResponse);
+dd($finalResponse);*/
+
+/** delete single media */
+/*$httpClient = new Client([
+    'headers' => ['Content-Type' =>'application/json','Authorization'=> $key],
+]);
+$sendRequestAndGetResponse = $httpClient->delete("https://api.wistia.com/v1/medias/tcg2euz6to.json");
+$finalResponse = json_decode($sendRequestAndGetResponse->getBody(), true);
+dd($finalResponse);*/
+
+/** get media status */
+/*$httpClient = new Client([
+    'headers' => ['Content-Type' =>'application/json','Authorization'=> $key],
+]);
+$sendRequestAndGetResponse = $httpClient->get("https://api.wistia.com/v1/medias/gd2ofy9v7j/stats.json");
+$finalResponse = json_decode($sendRequestAndGetResponse->getBody(), true);
+dd($finalResponse);*/
+
+/** upload file media to specific project by hashed id project */
+/*$request = [
+    'url' => 'https://avatars.githubusercontent.com/u/3670578?v=4',
+    'api_password' => '8c5bbd20b3600ce91f5fadda3a668cf267f3562c76daa21d5bae0846eefaf0a2',
+    'project_id' => '74io6qlc8d',
+    'name' => 'new new name',
+    'description' => 'new new media description'
+];
+$httpClient = new Client(['headers' => ['Content-Type' =>'application/x-www-form-urlencoded']]);
+try {
+    $sendRequestAndGetResponse = $httpClient->post("https://upload.wistia.com",[
+        'form_params' => $request
+    ]);
+    $finalResponse = json_decode($sendRequestAndGetResponse->getBody(), true);
+    dd($finalResponse);
+}catch (Exception $exception) {
+    dd($exception->getMessage());
+}*/
+
